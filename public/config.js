2110c41c7bb05253403b44d099a21735207ae742
Mario Reder <mario.reder@pm.me>
2025-10-09 09:08:47 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/niggasterx",
   "VITE_SEO_SITE_NAME": "crackerliquid",
   "VITE_SEO_SITE_DESCRIPTION": "An orderly and clean perpetuals DEX.",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/crackerliquid-0534/",
+  "VITE_SEO_SITE_URL": "https://crackerliquid.fun",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@crackerliquid_",
